2b48b4f7f36aecb6fc2dfe1427c9b0673d8ea98c
hashboy05 <AOHuang05@gmail.com>
2025-02-01 20:07:56 +0800

    added the 3D game "Sharp Shooter"

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] GameObject robotExplosionVFX;
+    [SerializeField] int startingHealth = 3;
+    int currentHealth;
+    GameManager gameManager;
+    void Awake(){
+        currentHealth = startingHealth;
+    }
+    void Start(){
+        gameManager = FindFirstObjectByType<GameManager>();
+        gameManager.AdjustEnemiesLeft(1);
+    }
+
+    public void TakeDamage(int amount){
+        currentHealth -= amount;
+        if (currentHealth <= 0){
+            SelfDestruct();
+        }
+    }
+
+    public void SelfDestruct(){
+        Instantiate(robotExplosionVFX, transform.position, Quaternion.identity);
+        gameManager.AdjustEnemiesLeft(-1);
+        Destroy(this.gameObject);
+    }
+}
